f1ca3f79d4878d54cc2913c9fdf06d0cbe986032
Corey Weathers <cweathers@twilio.com>
2019-09-03 19:30:42 +0000

    Adds the ASP.NET MVC project to answer this SO question - https://stackoverflow.com/questions/57594553/getting-parameters-from-twilio-call-without-using-aspnet-library

---
+++ CallSidWebApp/Controllers/StackOverflowController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio;
+
+namespace CallSidWebApp.Controllers
+{
+    public class StackOverflowController : Controller
+    {
+        // GET: StackOverflow
+        public ActionResult Index(string callSid)
+        {
+            string id = callSid;
+            return View();
+        }
+    }
+}
---
+++ CallSidWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CallSidWebApp.MvcApplication" Language="C#" %>
